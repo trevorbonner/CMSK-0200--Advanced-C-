8820a3c0b60ab127c2efa242d870edb81d9b30e1
bonnert4 <bonnert4@macewan.ca>
2023-06-14 19:07:31 -0600

    checkin

---
+++ Day4/GenericTestList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    public class GenericTestList<T>
+    {
+        public List<T> Container { get; set; }
+
+        public GenericTestList()
+        {
+            Container = new List<T>();
+        }
+
+        public void Add(T newItem)
+        {
+            Container.Add(newItem);
+        }
+
+        public void Update(T newValue, T oldValue)
+        {
+            for(int i = 0; i < Container.Count; i++)
+            {
+                if (Container[i] != null && Container[i].Equals(oldValue))
+                {
+                    Container[i] = newValue;
+                }
+            }
+        }
+
+        public void Remove(T removeItem)
+        {
+            if (Container.Contains(removeItem))
+            {
+                Container.Remove(removeItem);
+            }
+        }
+
+        public T CreateNew()
+        {
+            var newT = default(T);
+            Container.Add(newT);
+            return newT;
+        }
+    }
+}
---
+++ Day4/Program.cs
@@ -0,0 +1,16 @@
+﻿using Day4;
+
+var testList1 = new GenericTestList<int>();
+testList1.Add(1);
+testList1.Add(2);
+testList1.Remove(2);
+var number = testList1.CreateNew();
+
+var testListString = new GenericTestList<string>();
+testListString.Add("1");
+testListString.Add("2");
+testListString.Add("3");
+testListString.Remove("1");
+var newString = testListString.CreateNew();
+
+Console.ReadLine();
